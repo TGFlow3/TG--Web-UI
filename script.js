ea65f227108afc0fa5fd928bc817977033d11568
tralyonh4 <tralyonh4@gmail.com>
2025-09-07 13:12:07 -0500

    Edit file via web app

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => 
   // Dropdown menu logic for Home.html user icon
   const userIcon = document.getElementById('userIcon');
   const userDropdown = document.getElementById('userDropdown');
